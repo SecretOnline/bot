9ea8aa3f861c91caf3e2c940fc1b42aa6208f197
secret_online <me+github@secretonline.co>
2016-06-08 08:57:31 +1200

    Add bot invite link command

--- addons/core.js
+++ addons/core.js
@@ -7,12 +7,16 @@ var changeCharHelp = [
   'changes the character used to trigger commands',
   'default is `~`'
 ];
+var inviteHelp = 'gives you a link to invite the bot to your own discord server';
+
+var inviteLink = 'https://discordapp.com/oauth2/authorize?client_id=177875535391752192&scope=bot';
 
 function init(bot) {
   _bot = bot;
 
   _bot.registerCommand('reload', new _bot.Command(doReload, 'core', _bot.Command.PermissionLevels.OVERLORD, reloadHelp));
   _bot.registerCommand('change-char', new _bot.Command(changeCommandChar, 'core', _bot.Command.PermissionLevels.ADMIN, changeCharHelp));
+  _bot.registerCommand('bot-invite', new _bot.Command(getInviteLink, 'core', inviteHelp));
 }
 
 function doReload() {
@@ -32,6 +36,18 @@ function changeCommandChar(input) {
     return `command character changed to \`${character}\``;
   } else {
     return 'no character specified';
+  }
+}
+
+function getInviteLink(input) {
+  // Just return link if more than this command, otherwise give a bigger description
+  if (input.originalMessage.content.split(' ').length === 1) {
+    return [
+      'here\'s the link to invite secret_bot to your own Discord server',
+      inviteLink
+    ].join('\n');
+  } else {
+    return inviteLink;
   }
 }
 
