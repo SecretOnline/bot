6f46ca454f53bed1fd47ff8569c287defa4234c4
secret_online <me+github@secretonline.co>
2016-06-08 17:16:00 +1200

    Add reversing (and unreversing)
    Also added a test command that always rejects

--- addons/commands.js
+++ addons/commands.js
@@ -19,6 +19,8 @@ var rollHelp = [
   '~roll 5d6',
   '~roll 1d20'
 ];
+var unReverseHelp = 'takes a reversed string and puts it the right way around';
+var reverseHelp = 'takes a string and reverses the letter order';
 
 function init(bot) {
   bot.registerCommand('say', new bot.Command(say, 'default', sayHelp));
@@ -28,6 +30,8 @@ function init(bot) {
   bot.registerCommand('google', new bot.Command(getGoogleLink, 'default', linkHelp));
   bot.registerCommand('lmgtfy', new bot.Command(getLmgtfyLink, 'default', linkHelp));
   bot.registerCommand('roll', new bot.Command(getRoll, 'default', rollHelp));
+  bot.registerCommand('reverse', new bot.Command(reverse, 'default', reverseHelp));
+  bot.registerCommand('unreverse', new bot.Command(reverse, 'default', unReverseHelp));
   // Latins
   bot.registerCommand('secret_latin', new bot.Command(getSecretText, 'latin'));
   bot.registerCommand('trk_latin', new bot.Command(getTrkText, 'latin'));
@@ -45,6 +49,35 @@ function say(input) {
 
 function raw(input) {
   return input.raw;
+}
+
+/*
+ * Reverse taken from https://github.com/mathiasbynens/esrever/blob/master/src/esrever.js
+ * to deal with javascript's unicode encoding
+ */
+function reverse(input) {
+  return input.process()
+    .then((result) => {
+      var regexSymbolWithCombiningMarks = /(<%= allExceptCombiningMarks %>)(<%= combiningMarks %>+)/g;
+      var regexSurrogatePair = /([\uD800-\uDBFF])([\uDC00-\uDFFF])/g;
+      // Step 1: deal with combining marks and astral symbols (surrogate pairs)
+      result = result
+        // Swap symbols with their combining marks so the combining marks go first
+        .replace(regexSymbolWithCombiningMarks, function($0, $1, $2) {
+          // Reverse the combining marks so they will end up in the same order
+          // later on (after another round of reversing)
+          return reverse($2) + $1;
+        })
+        // Swap high and low surrogates so the low surrogates go first
+        .replace(regexSurrogatePair, '$2$1');
+      // Step 2: reverse the code units in the string
+      var ret = '';
+      var index = result.length;
+      while (index--) {
+        ret += result.charAt(index);
+      }
+      return ret;
+    });
 }
 
 function getWikiLink(input) {
--- addons/test.js
+++ addons/test.js
@@ -5,10 +5,20 @@ function init(bot) {
   _bot = bot;
 
   _bot.registerCommand('permtest', new _bot.Command(doTest, 'test', _bot.Command.PermissionLevels.ADMIN));
+  _bot.registerCommand('reject', new _bot.Command(rejecter, 'test', _bot.Command.PermissionLevels.OVERLORD));
 }
 
 function doTest() {
   return 'permission test passed';
+}
+
+function rejecter(input) {
+  return input.process()
+    .then((result) => {
+      return new Promise((resolve, reject) => {
+        reject(result);
+      });
+    });
 }
 
 
