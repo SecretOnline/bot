62ac9f5d8af6dbb96b70df20e982b62448c391ab
secret_online <me+github@secretonline.co>
2016-10-17 22:23:46 +1300

    Add help topics

--- addons/help.js
+++ addons/help.js
@@ -5,15 +5,69 @@ const Channel = require('../bot/Channel.js');
 let helpHelp = [
   'syntax: `~help [command]`',
   'finds help for the given command',
+  'extra info about bot is available through the help topics `~help topic`',
   'for a list of all commands, use `~commands`',
-  'for all commands in a group, use `~commands [group]`'
+  'for all commands in a group, use `~commands <group>`',
+  'to find out which group a command is in, use `~which <command>`'
 ];
 let whichHelp = [
   'syntax: `~which <command>`',
   'alows you to find out which command group a command belongs to',
+  'for more information about groups, use `~help groups`',
   'example usage:',
   '`~which which`'
 ];
+let topics = {
+  help: [
+    'secret_bot has support for commands to provide help and usage information',
+    'help is accessed by using `~help <command>`',
+    'e.g. `~help help`'
+  ],
+  groups: [
+    'command groups do what they say: they group commands together',
+    'the main benefit of this is for server admins, who can enable and disable groups',
+    'you can get a list of all commands in a group by using `~commands <group>`',
+    'to find out which group a particular command is in, use `~which <command>`. e.g. `~which help`',
+    '',
+    'if two commands have a conflicting name, then you must use the group to identify which one to use',
+    'for example: `~faces.lenny`, `~someothergroup.lenny`',
+    '',
+    'each server had a hidden group of the format `<connection>.<server>`, which is used by, among others, the custom commands addon',
+    'this ensures that custom commands will always work on their server'
+  ],
+  commands: [
+    'commands form the base of secret_bot',
+    'if you wanted a list of all commands available, use `~commands`. note that the result may be different depending on which server you type it in',
+    '',
+    'the first \'word\' of a message must be a valid command in order for secret_bot to process it',
+    '',
+    'commands are made of three parts: the prefix, the trigger, and arguments',
+    'prefixes and triggers are explained in thir own topics: `prefixes` and `triggers`',
+    'the arguments for a command are any text that follows',
+    'for example `~flip upside down` has the arguments `upside` and `down`',
+    'what a command does with the arguments it is given is up to that command',
+    '',
+    'many commands feature recursive processing. more can be read on that in the `recursive` topic'
+  ],
+  triggers: [
+    'no, not that type of triggers',
+    'in secret_bot, the trigger is the part of a command that says which command this is',
+    'as an example, in `~help` the trigger is `help`',
+    'some commands ask you to give a trigger. `~help`, for example, will give more specific help if you give it a trigger'
+  ],
+  prefixes: [
+    'the prefix is the part of a command before the trigger. usually it is a single character',
+    'by default it is a tilde `~`, but it can be changed per server',
+    'throughout the help, the tilde is used to denote a command, for example `~help topic prefixes`'
+  ],
+  recursive: [
+    'secret_bot features recursive command processing',
+    'that means the output from one command can be used as the arguments for another',
+    'for example, `~flip ~fliptable`',
+    '`~fliptable` returns a unicode emote. this is then given to `~flip` to turn upside down',
+    'as shown in that example, commands work from right to left, processing the \'last\' command in a message, finally making its way down to the first'
+  ]
+};
 
 class Help extends ScriptAddon {
   constructor(bot) {
@@ -23,7 +77,7 @@ class Help extends ScriptAddon {
   init() {
     this.bot.addCommand('commands', new Command(this.getCommands.bind(this), 'core', this.getCommands.bind(this)));
     this.bot.addCommand('help', new Command(this.getHelp.bind(this), 'core', helpHelp));
-    this.bot.addCommand('which', new Command(this.getWhich.bind(this), 'core', Command.PermissionLevels.ADMIN, whichHelp));
+    this.bot.addCommand('which', new Command(this.getWhich.bind(this), 'core', whichHelp));
   }
 
   deinit() {
@@ -100,24 +154,25 @@ class Help extends ScriptAddon {
         }
       }
 
+      let header = [
+        `secret_bot *help* -> *${input.text}*`
+      ];
+      let help = '';
+
       // Get command
       let comm = this.bot.getCommand(`${prefix}${input.text}`, input.message);
       if (comm) {
-        let help = comm.help(input.from(''));
+        header.push(`command group: ${comm.group}`);
 
+        help = comm.help(input.from(''));
         // Return on empty string. Allows for a no-response
         if (help === '') {
           return '';
         }
-
         if (!help) {
           help = `no help for ${input.text} can be found`;
         }
 
-        let header = [
-          `secret_bot *help* -> *${input.text}*`,
-          `command group: ${comm.group}`
-        ];
         if (comm.permission) {
           switch (comm.permission) {
             case 1:
@@ -128,21 +183,45 @@ class Help extends ScriptAddon {
               break;
           }
         }
-
-        response = [
-          ...header,
-          '',
-          help
-        ].join('\n');
-
       } else {
-        response = `unknown or disallowed command: ${input.text}`;
+        let parts = input.text.split(' ');
+        if (parts.shift() === 'topic') {
+          header = [
+            'secret_bot *help* -> *topic*'
+          ];
+          let rest = parts.join(' ');
+          if (!rest) {
+            help = [
+              'available help topics:',
+              Object.keys(topics).sort().map(item => `\`${prefix}${item}\``).join(', ')
+            ].join('\n');
+          } else if (topics[rest]) {
+            header[0] += ` -> *${rest}*`;
+            help = topics[rest].join('\n');
+          } else {
+            help = `no help topic named ${rest}`;
+          }
+        } else {
+          help = `unknown or disallowed command: ${input.text}`;
+        }
       }
+
+      response = [
+        ...header,
+        '',
+        help
+      ].join('\n');
     } else {
-      response = 'secret_bot *help*\n' +
-        'secret_bot v7.0.x - the objectively better update\n\n' +
-        'secret_bot is written by secret_online\n' +
-        'https://github.com/SecretOnline/bot';
+      response = [
+        'secret_bot *help*',
+        'secret_bot v7.0.x - the objectively better update',
+        '',
+        'help for individual commands can be found by using `~help <command>`',
+        'more help topics can be found at `~help topic`',
+        '',
+        'secret_bot is written by secret_online',
+        'https://github.com/SecretOnline/bot'
+      ].join('\n');
     }
 
     input.user.send(response);
