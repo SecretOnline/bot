d7ce9bfc6ba70db7e83a6f7be5d42fb9ebec9146
secret_online <me+github@secretonline.co>
2016-12-31 22:27:25 +1300

    Big huge i-forgot-to-commit commit
    I keep doing these. I really ned to get a hold of myself.

--- addons/summaries.js
+++ addons/summaries.js
@@ -1,12 +1,18 @@
 // This addon only works for Discord at the moment
-const url = require('url');
-
 const Discord = require('discord.js');
 const snoowrap = require('snoowrap');
 
 const ScriptAddon = require('../bot/ScriptAddon.js');
 const Command = require('../bot/Command.js');
-const request = require('../util/').request;
+
+const truncate = require('../util').truncate;
+
+// This is a monstrosity
+// Matches:
+//   redd.it/<id>
+//   reddit.com/r/<id>[/comments/<id>]
+//   <subreddit>.reddit.com/<id>
+let redditRegex = /https?:\/\/(?:(?:redd\.it\/(\w+))|(?:\w+\.)reddit\.com\/r\/\w+\/comments\/(\w+)(\/[^\s/]+\/\w+)?|(\w+\.)reddit\.com\/(\w+)\/)/;
 
 let redditHelp = [
   'Gets info about a reddit thread or comment'
@@ -16,7 +22,13 @@ class Summaries extends ScriptAddon {
   constructor(bot) {
     super(bot, 'summaries');
 
-    // TODO: Create reddit wrapper
+    this.snoo = new snoowrap({
+      userAgent: this.conf.reddit.useragent,
+      clientId: this.conf.reddit.id,
+      clientSecret: this.conf.reddit.secret,
+      refreshToken: this.conf.reddit.refresh,
+      accessToken: this.conf.reddit.access
+    });
   }
 
   init() {
@@ -27,8 +39,75 @@ class Summaries extends ScriptAddon {
     // Do nothing
   }
 
-  redditHelp(input) {
-    
+  redditSummary(input) {
+    return new Promise((resolve, reject) => {
+      if (input.message.connection.id !== 'djs') {
+        reject('for now, reddit summary is only available on Discord');
+        return;
+      }
+
+      let match = input.text.match(redditRegex);
+      if (!match) {
+        reject(`${input.text} isn't a reddit url`);
+        return;
+      }
+
+      let postId, commentId, post, comment;
+      // Short URL
+      if (match[1]) {
+        postId = match[1];
+      } else
+      // Normal URL
+      if (match[2]) {
+        postId = match[2];
+        commentId = match[3];
+      } else
+      // subreddit subdomain
+      if (match[4]) {
+        postId = match[5];
+      } else {
+        reject('expression error trying to get reddit summary');
+        return;
+      }
+
+      post = this.snoo.getSubmission(postId);
+      if (commentId) {
+        comment = this.snoo.getComment(commentId);
+      }
+
+      let promises = [
+        post.fetch(),
+        post.author.fetch()
+      ];
+
+      Promise.all(promises)
+        .then(resolve, (err) => {
+          reject('unable to get reddit summary');
+        });
+    })
+      .then((res) => {
+        console.log(res);
+        let post = res[0];
+        let author = res[1];
+        let embed = new Discord.RichEmbed()
+          .setTitle(post.title)
+          .setAuthor(`/r/${post.subreddit.display_name}: ${author.name}`)
+          .setURL(post.url)
+          .addField('Score',`**${post.score}** points\n${post.ups} upvotes`, true)
+          .addField(`${post.num_comments} comments`, '\u200b', true);
+
+        if (post.is_self) {
+          embed.setDescription(truncate(post.selftext));
+        } else {
+          if (post.thumbnail) {
+            embed.setThumbnail(post.thumbnail);
+          }
+        }
+
+        input.message.connection.discord.channels.get(input.message.channel.id).sendEmbed(embed);
+
+        return '';
+      });
   }
 }
 
