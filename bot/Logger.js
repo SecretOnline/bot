ebec1d4d847204496d8dac68a687398da7a83ac5
secret_online <me+github@secretonline.co>
2017-01-13 19:08:20 +1300

    Add logger start function

--- bot/Logger.js
+++ bot/Logger.js
@@ -15,10 +15,41 @@ class Logger {
    * @memberOf Logger
    */
   constructor(bot, directory) {
-
+    this.bot = bot;
+    this.dir = directory;
+    this.logs = null;
+    this._ready = new Promise((resolve, reject) => {
+      // Store resolution function so we can call it in .start()
+      this._readyFunc = resolve;
+    });
   }
   
   //region Functions
+
+  start() {
+    return this.bot._listDirectory(this.dir)
+      .then((files) => {
+        let exp = /^(\w+)\.(\d+).log$/;
+        let matches = files
+          .map(f => f.match(exp)) // Match all files
+          .filter(f => !!f); // Remove failed matches
+
+        this.logs = matches.reduce((logMap, match) => {
+          let id = match[1];
+          let num = Number.parseInt(match[2]);
+
+          // Set highest number for id
+          if ((!logMap.has(id)) || (logMap.get(id) < num)) {
+            logMap.set(id, num);
+          }
+
+          return logMap;
+        }, new Map());
+      })
+      .then(() => {
+        this._readyFunc();
+      });
+  }
   
   getLogs(location, limit = 80) {
 
