27302193d2a4492e3c28ae492333e0ad34afa0ee
secret_online <me+github@secretonline.co>
2017-01-30 17:03:06 +1300

    Create history file

---
+++ addons/history.js
@@ -0,0 +1,13 @@
+const ScriptAddon = require('../bot/ScriptAddon.js');
+
+class History extends ScriptAddon {
+  constructor(bot) {
+    super(bot, 'history');
+  }
+
+  init() {
+    
+  }
+}
+
+module.exports = History;
