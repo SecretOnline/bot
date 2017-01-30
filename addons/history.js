8224858defa1dc6834cfaf06d0269596e7820206
secret_online <me+github@secretonline.co>
2017-01-30 19:21:54 +1300

    Add history function
    Just gets the last n messages

--- addons/history.js
+++ addons/history.js
@@ -1,4 +1,9 @@
+const Discord = require('discord.js');
+
 const ScriptAddon = require('../bot/ScriptAddon.js');
+const Logger = require('../bot/Logger.js');
+
+const {truncate} = require('../util');
 
 const historyHelp = [
   'syntax: `~history <number-of-messages> [user-mention]`',
@@ -16,7 +21,68 @@ class History extends ScriptAddon {
   }
 
   getHistory(input) {
-    
+    let length = 10;
+    if (!input.args.length) {
+      throw 'no number of messages given';
+    }
+
+    if (!input.args[0].match(/^\d+$/)) {
+      throw `${input.args[0]} is not a number`;
+    }
+
+    let num = Number.parseInt(input.args[0]);
+    if (num < 0) {
+      throw 'must have a positive number of messages to retrieve';
+    }
+    if (num > 10) {
+      num = 10;
+    }
+    length = num;
+
+    let filter;
+    if (input.message.mentions.users.size) {
+      let user = input.message.mentions.users.first();
+      let channel = input.message.channel;
+      filter = (line) => {
+        if (line.type !== 'message') {
+          return false;
+        }
+        if (line.author !== user.id) {
+          return false;
+        }
+        if (line.channel !== channel.id) {
+          return false;
+        }
+        
+        return true;
+      };
+    } else {
+      filter = Logger.filterByChannel(input.message.channel);
+    }
+
+    return this.bot.getLogs(filter, length)
+      .then((lines) => {
+        let plural = length > 1;
+        let embed = new Discord.RichEmbed()
+          .setTitle(`Last ${plural?`${length} `:''}message${plural?'s':''}`)
+          .setColor('#F1C40F');
+
+        lines.forEach((line) => {
+          let text;
+          if (line.embed) {
+            if (line.message) {
+              text = `${line.message} {embed}`;
+            } else {
+              text = '{embed}';
+            }
+          } else {
+            text = line.message;
+          }
+          embed.addField(line.username, truncate(text, 80), true);
+        });
+
+        return embed;
+      });
   }
 }
 
