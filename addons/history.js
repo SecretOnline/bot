5c1ff6ddf0e7d5bd8cfb74d583467018d0d20b72
secret_online <me+github@secretonline.co>
2017-01-30 17:08:32 +1300

    Add function outline and help for ~history
    No actual functionality yet, something has to be done first

--- addons/history.js
+++ addons/history.js
@@ -1,11 +1,21 @@
 const ScriptAddon = require('../bot/ScriptAddon.js');
 
+const historyHelp = [
+  'syntax: `~history <number-of-messages> [user-mention]`',
+  'gets the last <number> messages spoken in this channel',
+  'adding a user mention filters to messages said by that person'
+];
+
 class History extends ScriptAddon {
   constructor(bot) {
     super(bot, 'history');
   }
 
   init() {
+    this.addCommand('history', this.getHistory, historyHelp);
+  }
+
+  getHistory(input) {
     
   }
 }
