008792d8b6bef9f20dad45cb944a123aff8fe658
secret_online <me+github@secretonline.co>
2017-02-02 21:28:40 +1300

    lowercase cleverbot replies
    also lowercase commit mesage
    meta

--- addons/conversation.js
+++ addons/conversation.js
@@ -129,7 +129,7 @@ class ConversationAddon extends ScriptAddon {
                 reject(err);
                 return;
               }
-              resolve(response);
+              resolve(response.toLowerCase());
             });
           });
     });
