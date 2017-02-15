4a9007641a4d9389dee03312cb9c0811f068439b
secret_online <me+github@secretonline.co>
2017-02-15 18:00:43 +1300

    Move string splitting outside of Input

--- util/index.js
+++ util/index.js
@@ -3,11 +3,13 @@ const request = require('./request.js');
 const truncate = require('./truncate.js');
 const arrayRandom = require('./arrayRandom.js');
 const MarkovChain = require('./markov.js');
+const quoteSplit = require('./quoteSplit.js');
 
 module.exports = {
   promprint,
   request,
   truncate,
   arrayRandom,
-  MarkovChain
+  MarkovChain,
+  quoteSplit
 };
