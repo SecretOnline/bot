20cc5506c07556489d0a3e59242c778e5a913e25
secret_online <me+github@secretonline.co>
2017-02-25 20:33:00 +1300

    Add data to ReAction

--- bot/ReAction.js
+++ bot/ReAction.js
@@ -1,5 +1,20 @@
+/**
+ * An action to be taken when reacted to
+ * 
+ * @class ReAction
+ */
 class ReAction {
-  constructor() {
+  /**
+   * Creates an instance of ReAction.
+   * 
+   * @param {(string|function)} action Action to be taken then reacted to
+   * 
+   * @memberOf ReAction
+   */
+  constructor(input, action) {
+    this._input = input;
+    this._action = action;
+  }
 
   }
 }
