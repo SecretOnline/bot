cf45ff2ae166b52e44ee7f5564cc04efa1d039cc
secret_online <me+github@secretonline.co>
2017-02-25 20:33:34 +1300

    Make ReActions act

--- bot/ReAction.js
+++ bot/ReAction.js
@@ -1,3 +1,5 @@
+const InputOverride = require('./InputOverride');
+
 /**
  * An action to be taken when reacted to
  * 
@@ -15,7 +17,27 @@ class ReAction {
     this._input = input;
     this._action = action;
   }
+  
+  act(user, channel) {
+    let str;
+    let over = new InputOverride('', user, channel);
 
+    if (typeof this._action === 'string') {
+      str = this._action;
+    } else if (typeof this._action === 'function') {
+      let fRes = this._action(this._input.from(over));
+      if (typeof fRes === 'string') {
+        str = fRes;
+      } else if (fRes instanceof Promise) {
+        return fRes;
+      }
+    }
+
+    over = over.merge(new InputOverride(str));
+
+    return this._input
+      .from(over)
+      .process();
   }
 }
 
