ac993b0780f7ac69bf44b4551882d99ee7d6b50a
secret_online <me+github@secretonline.co>
2017-03-08 19:22:45 +1300

    Add ~maths and ~math

--- addons/maths.js
+++ addons/maths.js
@@ -1,8 +1,24 @@
+const mathjs = require('mathjs');
+
 const ScriptAddon = require('../bot/ScriptAddon.js');
+
+const mathsHelp = [
+  'evaluates mathematical expressions',
+  'right now, it doesn\'t include the calculus features of math.js, but it does do the core basic operations',
+  'it also does trigonometry, matricies, and complex numbers',
+  'in case you did need help for your calc, `(nx)^m => (nmx)^(m-1)`',
+  'note: some small errors may occur due to the way computers handle numbers. this is unavoidable',
+  'examples:',
+  '`~maths 1 + 1`',
+  '`~maths sin(pi / 4) ^ 2`',
+  '`~maths e ^ (pi * i)'
+];
 
 class Maths extends ScriptAddon {
   constructor(bot) {
     super(bot, 'maths');
+
+    this.maths = mathjs.parser();
   }
 
   get description() {
@@ -10,7 +26,24 @@ class Maths extends ScriptAddon {
   }
 
   init() {
+    this.addCommand('maths', this.getMathsResult);
+    this.addCommand('math', this.getMathsResult);
+  }
 
+  getMathsResult(input) {
+    return input.process()
+      .then((res) => {
+        let result;
+        try {
+          result = this.maths.eval();
+          if (typeof result !== 'string') {
+            result = result.toString();
+          }
+        } catch (err) {
+          result = err.message;
+        }
+        return result;
+      });
   }
 }
 
