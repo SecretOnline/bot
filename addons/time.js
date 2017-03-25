52b1507debe40ae5e491415df9b094224f221d07
secret_online <me+github@secretonline.co>
2017-03-26 12:34:23 +1300

    Add getting and setting of timezones

--- addons/time.js
+++ addons/time.js
@@ -1,11 +1,27 @@
 const Discord = require('discord.js');
 
 const ScriptAddon = require('../bot/ScriptAddon.js');
+const Result = require('../bot/Result.js');
+const {ReAction} = Result;
 
 const {resolveMentions} = require('../util');
 
-const setTimeHelp = [
-  'syntax: `~set-time `'
+const listLink = 'https://wikipedia.org/wiki/List_of_tz_database_time_zones#List';
+
+const setTimeHelp = {
+  text: [
+    'syntax: `~set-time <timezone>`',
+    'the timezone must be in `Country/City` or `Continent/Country or State/City` format, from the list below',
+    'the `Etc/GMT` timezones are not supported'
+  ],
+  actions: [
+    {emoji: 'clock7', desc: 'view a list of timezone names (use the TZ column)', action: listLink}
+  ]
+};
+const getTimeHelp = [
+  'syntax: `~time [user]`',
+  'gets the time for a user based on the timezone they have set',
+  'if no user is given, it uses your time'
 ];
 
 class Time extends ScriptAddon {
@@ -18,21 +34,46 @@ class Time extends ScriptAddon {
   }
 
   init() {
-
+    this.addCommand('set-time', this.setTime, setTimeHelp);
+    this.addCommand('time', this.getTime, getTimeHelp);
   }
 
   setTime(input) {
-    input.process()
+    return input.process()
       .then((res) => {
-        let user = input.user;
-        let conf = this.getConfig(input.channel.guild);
+        let conf = this.getUser(input.user);
 
-        
+        let tz = res.args[0];
+        if (!tz) {
+          let r = new Result(true);
+          r.add('no timezone was given');
+          r.add(new ReAction('clock7', 'view a list of timezone names (use the TZ column). Note: only `Continent/City` or `Continent/Country or State/City` zones work', input, listLink));
+
+          return r;
+        }
+
+        // Test the timezone before going anywhere
+        // If it's invalid, the toLocaleTimeString will throw
+        try {
+          new Date().toLocaleTimeString('en-US', {timeZone:tz});
+        } catch (err) {
+          let r = new Result(true);
+          r.add(`${tz} is not a valid timezone`);
+          r.add(new ReAction('clock7', 'view a list of timezone names (use the TZ column). Note: only `Continent/City` or `Continent/Country or State/City` zones work', input, listLink));
+
+          return r;
+        }
+
+        conf.tz = tz;
+        return this.setUser(conf, input.user)
+          .then(() => {
+            return `changed your timezone to ${tz}`;
+          });
       });
   }
 
   getTime(input) {
-    input.process()
+    return input.process()
       .then((res) => {
         let user;
 
@@ -47,14 +88,14 @@ class Time extends ScriptAddon {
         } else {
           user = input.user;
         }
-        
 
-        let conf = this.getConfig(input.channel.guild);
-        if (conf[user.id]) {
+        let conf = this.getUser(input.user);
+        if (conf.tz) {
+          return `the time for ${input.user} is ${new Date().toLocaleTimeString('en-US', {timeZone:conf.tz})}`;
           // do time stuff
         } else {
           if (user.id === input.user.id) {
-            throw 'you have not added your timezone. use `~set-time +/-hours` to use `~time`';
+            throw 'you have not added your timezone. use `~set-time <timezone>` to use `~time`';
           }
           throw `${user.toString()} has not set their time`;
         }
