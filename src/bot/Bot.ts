824784dccb47da8deed1e2cd5f3e75376f0f6db9
SecretOnline <me+github@secretonline.co>
2017-09-26 19:22:03 +1300

    List directories in bot

--- src/bot/Bot.ts
+++ src/bot/Bot.ts
@@ -12,8 +12,23 @@ interface BotConfig {
 
 export default class Bot {
   private config: BotConfig;
+  private connections = new Map<string, Connection>();
 
   async start(config: BotConfig) {
     this.config = config;
+  }
+
+  private listDirectory(path: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+      readdir(path, (err, data) => {
+        if (err) {
+          // this.error(`unable to read directory ${path}`);
+          reject(err);
+          return;
+        }
+
+        resolve(data);
+      });
+    });
   }
 }
